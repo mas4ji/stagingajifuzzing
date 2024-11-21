a948a0ee5fdd783e4810fe1d9ee5a8faad9e3966
Mas Aji <127058700+mas4ji@users.noreply.github.com>
2024-11-21 20:00:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Pastikan file skrip utama (misalnya AjiFuzzer.sh) ada
-if [ ! -f "ajifuzzer.sh" ]; then
-    echo "File AjiFuzzer.sh tidak ditemukan. Pastikan file ini ada di direktori yang sama dengan skrip install.sh"
+# Pastikan file skrip utama (misalnya ajistaging.sh) ada
+if [ ! -f "ajistaging.sh" ]; then
+    echo "File ajistaging.sh tidak ditemukan. Pastikan file ini ada di direktori yang sama dengan skrip install.sh"
     exit 1
 fi
 
-# Tentukan nama perintah yang diinginkan
-command_name="ajifuzz"  # Ganti dengan nama perintah yang diinginkan (misalnya 'ajifuzz')
-
-# Menyalin file skrip utama (AjiFuzzer.sh) ke direktori /usr/bin dengan nama yang sesuai
-sudo cp AjiFuzzer.sh "/usr/bin/$command_name"
+# Menyalin file skrip utama (ajistaging.sh) ke direktori /usr/bin dengan nama ajifuzz
+sudo cp ajistaging.sh /usr/bin/ajifuzz
 
 # Memberikan izin eksekusi pada file tersebut
-sudo chmod u+x "/usr/bin/$command_name"
+sudo chmod u+x /usr/bin/ajifuzz
 
 # Menampilkan pesan sukses
-echo "$command_name telah berhasil diinstal! Sekarang Anda dapat menjalankan alat ini dengan perintah '$command_name'."
+echo "Ajistaging telah berhasil diinstal! Sekarang Anda dapat menjalankan alat ini dengan perintah 'ajifuzz'."
