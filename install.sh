4dbc6480e39d7576e22f15862efa45cb2b6cd9ea
Mas Aji <127058700+mas4ji@users.noreply.github.com>
2024-11-22 09:59:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Menyalin staging.sh ke /usr/local/bin/staging (perbaiki jalur ke /usr/local/bin)
-sudo cp staging.sh /usr/local/bin/staging
+# Pastikan file skrip utama (misalnya AjiFuzzer.sh) ada
+if [ ! -f "staging.sh" ]; then
+    echo "File staging.sh tidak ditemukan. Pastikan file ini ada di direktori yang sama dengan skrip install.sh"
+    exit 1
+fi
 
-# Memberikan izin eksekusi pada file staging
-sudo chmod u+x /usr/local/bin/staging
+# Menyalin file skrip utama (AjiFuzzer.sh) ke direktori /usr/bin dengan nama aji
+sudo cp staging.sh /usr/bin/staging
+
+# Memberikan izin eksekusi pada file tersebut
+sudo chmod u+x /usr/bin/staging
 
 # Menampilkan pesan sukses
-echo "Staging telah terinstal dengan sukses! Sekarang jalankan perintah 'staging' untuk menggunakan alat ini."
+echo "AjiFuzzer telah berhasil diinstal! Sekarang Anda dapat menjalankan alat ini dengan perintah 'aji'."
