b8f503c18bf469fd78d55b864f99b24676660c2a
Mas Aji <127058700+mas4ji@users.noreply.github.com>
2024-11-22 09:55:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Menyalin staging.sh ke /usr/bin/staging (memastikan ada garis miring)
-sudo cp staging.sh /usr/bin/staging
+# Menyalin staging.sh ke /usr/local/bin/staging (perbaiki jalur ke /usr/local/bin)
+sudo cp staging.sh /usr/local/bin/staging
 
 # Memberikan izin eksekusi pada file staging
-sudo chmod u+x /usr/bin/staging
+sudo chmod u+x /usr/local/bin/staging
 
 # Menampilkan pesan sukses
 echo "Staging telah terinstal dengan sukses! Sekarang jalankan perintah 'staging' untuk menggunakan alat ini."
